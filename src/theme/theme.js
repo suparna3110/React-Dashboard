4952231615120001cbd216f84ee5a2263d738306
suparna3110 <suparna.arora@iaugmentor.com>
2020-07-25 13:40:14 +0530

    Add Typography and font style

---
+++ src/theme/theme.js
@@ -0,0 +1,28 @@
+import { createTheme } from "@fluentui/react";
+
+export default createTheme({
+  palette: {
+    themePrimary: "#e84533",
+    themeLighterAlt: "#090302",
+    themeLighter: "#250b08",
+    themeLight: "#46150f",
+    themeTertiary: "#8b291f",
+    themeSecondary: "#cc3d2d",
+    themeDarkAlt: "#ea5646",
+    themeDark: "#ee6f61",
+    themeDarker: "#f29388",
+    neutralLighterAlt: "#211e1e",
+    neutralLighter: "#2a2626",
+    neutralLight: "#393434",
+    neutralQuaternaryAlt: "#423c3c",
+    neutralQuaternary: "#494343",
+    neutralTertiaryAlt: "#686161",
+    neutralTertiary: "#c8c8c8",
+    neutralSecondary: "#d0d0d0",
+    neutralPrimaryAlt: "#dadada",
+    neutralPrimary: "#ffffff",
+    neutralDark: "#f4f4f4", 
+    black: "#f8f8f8",
+    white: "#171515",
+  },
+});
